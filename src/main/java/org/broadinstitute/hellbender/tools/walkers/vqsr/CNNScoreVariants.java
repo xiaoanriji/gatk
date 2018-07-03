2a34d4b96857ccadda679746b3ecb66a240821ac
Chris Norman <cnorman@broadinstitute.org>
2018-07-03 10:10:26 -0400

    Replace VariantFilter lambda with named classes and tests.

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNScoreVariants.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNScoreVariants.java
@@ -14,7 +14,7 @@ import org.broadinstitute.hellbender.engine.filters.*;
 import org.broadinstitute.hellbender.exceptions.GATKException;
 import org.broadinstitute.hellbender.utils.haplotype.HaplotypeBAMWriter;
 import org.broadinstitute.hellbender.utils.io.IOUtils;
-import  org.broadinstitute.hellbender.utils.io.Resource;
+import org.broadinstitute.hellbender.utils.io.Resource;
 import org.broadinstitute.hellbender.utils.python.StreamingPythonScriptExecutor;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
 import org.broadinstitute.hellbender.utils.runtime.AsynchronousStreamWriter;
@@ -203,12 +203,12 @@ public class CNNScoreVariants extends VariantWalker {
     }
 
     @Override
-    protected CountingVariantFilter makeVariantFilter(){
-        if (filterSymbolicAndSV) {
-            return new CountingVariantFilter(VariantFilterLibrary.NOT_SV_OR_SYMBOLIC);
-        } else {
-            return new CountingVariantFilter(VariantFilterLibrary.ALLOW_ALL_VARIANTS);
-        }
+    protected CountingVariantFilter makeVariantFilter() {
+        return new CountingVariantFilter(
+                filterSymbolicAndSV ?
+                        VariantFilterLibrary.NOT_SV_OR_SYMBOLIC:
+                        VariantFilterLibrary.ALLOW_ALL_VARIANTS
+        );
     }
 
     @Override
